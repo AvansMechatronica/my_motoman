ec4902c704de75f7cfc843f542cf6eb22f760fd6
Gerard Harkema <GerardAnneHarkema@gmail.com>
2026-02-02 17:08:09 +0100

    added new moveit config

--- my_motoman_moveit_config/launch/moveit_rviz.launch.py
+++ my_motoman_moveit_config/launch/moveit_rviz.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_moveit_rviz_launch
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("my_ur", package_name="my_motoman_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("motoman_hc10", package_name="my_motoman_moveit_config").to_moveit_configs()
     return generate_moveit_rviz_launch(moveit_config)
--- my_motoman_moveit_config/launch/static_virtual_joint_tfs.launch.py
+++ my_motoman_moveit_config/launch/static_virtual_joint_tfs.launch.py
@@ -3,5 +3,5 @@ from moveit_configs_utils.launches import generate_static_virtual_joint_tfs_laun
 
 
 def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("my_ur", package_name="my_motoman_moveit_config").to_moveit_configs()
+    moveit_config = MoveItConfigsBuilder("motoman_hc10", package_name="my_motoman_moveit_config").to_moveit_configs()
     return generate_static_virtual_joint_tfs_launch(moveit_config)
